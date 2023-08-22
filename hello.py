a6e10056d9faf7c64513b2ca5887cf90ae438aff
James Ward <jimmywardmichael@gmail.com>
2023-08-22 12:12:12 -0500

    slicing in python

--- hello.py
+++ hello.py
@@ -5,3 +5,19 @@
 fname = input("What is your first name?")
 lname = input("What is your last name?")
 print( fname + lname)
+# Slicing
+#Count the positions of letters
+
+ballparkearnings = "1,201,411"
+money = ballparkearnings[2:9]
+print(money)
+
+#You can also slice a string by just using the number position followed by : if u want remaining part of string
+#Using number: will print whatever position and remaining
+
+food = "I am hungry, I am not hungry"
+print(food[12:])
+
+#Using :number will print to whatever position is called and stops
+food1 = "Hotdogs, Hamburgers"
+print(food1[:7])
