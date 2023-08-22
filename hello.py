4aad4cf9a059f5c7dcd57d32313c9f22ac43b6ea
James Ward <jimmywardmichael@gmail.com>
2023-08-22 11:24:41 -0500

    init python project

---
+++ hello.py
@@ -0,0 +1,7 @@
+# print("Hello, World!")
+# name = input("What is your name?")
+# print(name)
+
+fname = input("What is your first name?")
+lname = input("What is your last name?")
+print( fname + lname)
